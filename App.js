cceb804b08960acd31e65e1ceca738d977657251
Prachan <prachanshetty@gmail.com>
2025-01-21 12:56:55 +0530

    React - Episode1

---
+++ App.js
@@ -0,0 +1,12 @@
+
+const heading = React.createElement("div",{id:"parent"},
+    [React.createElement("div",{id:"child1"}, 
+       [React.createElement("h1",{},"Child1 h1 tah"),  React.createElement("h2",{},"child1 h2 tag")]),
+    React.createElement("div",{id:"child2"}, 
+       [React.createElement("h1",{},"Child2 h1 tah"),  React.createElement("h2",{},"child2 h2 tag")]),
+    ]
+);
+console.log(heading)
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(heading);
